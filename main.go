4a3fca2a0a1ce718a528ba8cdbd83b5cc20823a5
B3stGRands <62728710+B3stGRands@users.noreply.github.com>
2020-03-26 19:09:28 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+  "encoding/json"
+  "log"
+  "net/http"
+  )
+  
+  type citiesResponse struct {
+    Cities []string `json:"cities"` // Cities capitalised to export it, otherwise json will ignore it.
+    }
+    
+    func cityHandler (res http.ResponseWrite, req *http.Request) {
+      cities := citiesResponse{
+        Cities: []string{"Amsterdam", "Berlin", "New York", "San Francisco", "Tokyo"}}
+      
+      res.Header().Set("Content-Type", "appliction/json; charset=utf-8")
+      json.NewEncoder(res).Encode(cities)
+      }
+     
+func defaultHandler(res http.ResponseWriter, req *httpRequest) {
+    res.Header().Set("Content-Type", "test/plain; charset=utf-8")
+    res.Write([]byte("Hello world!!!"))
+}
+
+func main() {
+    http.HandleFunc("/", defaultHandler)
+    http.HandleFunc("/cities.json", cityHandler)
+    err := http.ListenAndServe(":5000", nil)
+    if err !=nil {
+        log.Fatal("Unable to listen on port 5000 : ", err)
+    }
+}
+
+     
